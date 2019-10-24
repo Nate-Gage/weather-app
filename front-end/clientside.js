2bfc3497e894af29cfab0c3ca6bff4c53b98a485
Nate <nate@Nates-MacBook-Air.local>
2019-10-23 18:55:28 -0600

    synced HTML form with client side JS to display location and forecast data

--- front-end/clientside.js
+++ front-end/clientside.js
@@ -7,13 +7,30 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
     })
 })
 
-fetch('http://localhost:3000/sandbox?address=kona').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log('there was an error')
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+const forecastText = document.querySelector('#forecastMessage')
+
+//messageOne.textContent = 'From Javascript!'
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageTwo.textContent = 'Loading...'
+
+    fetch('http://localhost:3000/sandbox?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log('Error. Please enter a valid address')
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+                forecastText.textContent = 'Here is your location: ' + data.location + data.forecast
+            }
+        })
     })
 })
